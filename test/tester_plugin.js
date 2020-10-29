ee8a725bece996021a48dbff574ceaa5f4e01866
Huang Rui <vowstar@gmail.com>
2020-10-29 21:33:05 +0800

    test/tester_plugin.js: format code, use 2 spaces
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- test/tester_plugin.js
+++ test/tester_plugin.js
@@ -1,17 +1,17 @@
 var Q = require('q');
 
 function processBlock(blk) {
-    var deferred = Q.defer();
-    deferred.resolve("from plugin!");
-    return deferred.promise;
+  var deferred = Q.defer();
+  deferred.resolve("from plugin!");
+  return deferred.promise;
 }
 
 module.exports = {
-    blocks: {
-        test: {
-            process: processBlock
-        }
-    },
-    hooks: {
+  blocks: {
+    test: {
+      process: processBlock
     }
+  },
+  hooks: {
+  }
 };
