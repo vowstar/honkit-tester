bcfcd6f596639850c458805391f302dc849dc3bd
Huang Rui <vowstar@gmail.com>
2020-11-28 17:01:34 +0800

    test: fix `withLocalPlugin` doesn't work properly with scoped names
    Issue: https://github.com/vowstar/honkit-tester/issues/1
    
    Signed-off-by: Huang Rui <vowstar@gmail.com>

---
+++ test/plugin_scoped/tester_plugin.js
@@ -0,0 +1,17 @@
+var Q = require('q');
+
+function processBlock(blk) {
+  var deferred = Q.defer();
+  deferred.resolve("from plugin!");
+  return deferred.promise;
+}
+
+module.exports = {
+  blocks: {
+    test: {
+      process: processBlock
+    }
+  },
+  hooks: {
+  }
+};
